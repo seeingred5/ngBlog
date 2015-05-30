d65d326b39b24f0b2b350729f394f5bde2f7dc8d
Tom <midson.trc@gmail.com>
2015-05-30 23:51:56 +1000

    first

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/ngblog-dev'
+  },
+
+  seedDB: true
+};
